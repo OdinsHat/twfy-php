517f14f0e0451b4642e64304019750e236294032
Doug Bromley <doug@tintophat.com>
2021-08-27 10:00:53 +0100

    PHPdoc changes

--- src/Twfy.php
+++ src/Twfy.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Tyre Label Generator.
+ * This file is part of TheyWorkForYou PHP SDK.
  * (c) Doug Bromley <doug@tintophat.com>
- * This source file is subject to the BSD license that is bundled
- * with this source code in the file LICENSE.
+ * This source file is subject to the BSD 3 Clause License that
+ * is bundled with this source code in the file LICENSE.
  */
 
 namespace OdinsHat\Twfy;
